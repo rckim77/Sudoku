6f34ecd7e4ee8568633c2ea1c09d193ba0461568
rckim77 <rckim77@gmail.com>
2020-06-04 00:32:54 -0400

    refactors button text logic

--- Sudoku/Row.swift
+++ Sudoku/Row.swift
@@ -24,21 +24,21 @@ struct Row: View {
             Button(action: {
                 self.updateSelectedButton(columnIndex: 0)
             }) {
-                RowButtonText(text: shouldClearText(columnIndex: 0) ? "" : "1")
+                RowButtonText(text: setRowButtonText(columnIndex: 0))
             }
                 .border(Color.black, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
             .background(isSelected(columnIndex: 0) ? Color.gray.opacity(0.4) : Color.white)
             Button(action: {
                 self.updateSelectedButton(columnIndex: 1)
             }) {
-                RowButtonText(text: shouldClearText(columnIndex: 1) ? "" : "2")
+                RowButtonText(text: setRowButtonText(columnIndex: 1))
             }
                 .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
             .background(isSelected(columnIndex: 1) ? Color.gray.opacity(0.4) : Color.white)
             Button(action: {
                 self.updateSelectedButton(columnIndex: 2)
             }) {
-                RowButtonText(text: shouldClearText(columnIndex: 2) ? "" :  "3")
+                RowButtonText(text: setRowButtonText(columnIndex: 2))
             }
                 .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
                 .background(isSelected(columnIndex: 2) ? Color.gray.opacity(0.4) : Color.white)
@@ -52,8 +52,25 @@ struct Row: View {
             selectedCell.coordinate?.square == squareIndex
     }
 
-    private func shouldClearText(columnIndex: Int) -> Bool {
-        return isSelected(columnIndex: columnIndex) && userAction.action == .clear
+    private func setRowButtonText(columnIndex: Int) -> String {
+        let shouldClearText = isSelected(columnIndex: columnIndex) && userAction.action == .clear
+
+        guard !shouldClearText else {
+            return ""
+        }
+
+        if index == 0 {
+            let digit = 1 + columnIndex
+            return String(describing: digit)
+        } else if index == 1 {
+            let digit = 4 + columnIndex
+            return String(describing: digit)
+        } else if index == 2 {
+            let digit = 7 + columnIndex
+            return String(describing: digit)
+        }
+
+        return ""
     }
 
     private func updateSelectedButton(columnIndex: Int) {
