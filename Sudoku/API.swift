f8ed0ce3c2c0b3dd642ef744e9eb190d54551051
Ray Kim <rkim@classpass.com>
2023-12-23 14:28:22 -0500

    refactors API code

--- Sudoku/API.swift
+++ Sudoku/API.swift
@@ -9,33 +9,45 @@
 import Foundation
 
 struct API {
+    
+    enum APIError: Error {
+        case invalidURL
+    }
+    
+    /// Helper method for POST requests sending JSON with basic error handling
+    static func postURLRequest(url: String, requestBody: [String: Any]) throws -> URLRequest {
+        guard let url = URL(string: url) else {
+            throw APIError.invalidURL
+        }
+        
+        let jsonData = try JSONSerialization.data(withJSONObject: requestBody)
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpBody = jsonData
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        return urlRequest
+    }
+
     static func getHint(grid: [CoordinateValue]) async throws -> ChatResponse? {
         
         // set to your ChatGPT API secret key (otherwise hint functionality won't work)
         let bearerToken = ""
-
-        guard let url = URL(string: "https://api.openai.com/v1/chat/completions") else {
-            return nil
-        }
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-
         var prompt: [[String: String]] = []
         let stringGrid = GridFactory.stringGridFor(grid: grid)
         let content = "Provide a helpful clue specific to the following sudoku puzzle:\n \(stringGrid)"
 
         prompt.append(["role": "user", "content": content])
         
-        let jsonbody: [String: Any] = [
+        let requestBody: [String: Any] = [
             "model": "gpt-3.5-turbo",
             "messages": prompt
         ]
-        let jsonData = try? JSONSerialization.data(withJSONObject: jsonbody)
+        
+        var urlRequest = try postURLRequest(url: "https://api.openai.com/v1/chat/completions", requestBody: requestBody)
 
-        urlRequest.httpBody = jsonData
         urlRequest.addValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
         let (data, _) = try await URLSession.shared.data(for: urlRequest)
         
