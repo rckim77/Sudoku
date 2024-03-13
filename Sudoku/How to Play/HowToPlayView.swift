203eaaaed08344e523992bbd84da18287d0ac4b7
Ray Kim <rkim@classpass.com>
2024-03-13 03:13:24 -0400

    improves layout for ipad

--- Sudoku/How to Play/HowToPlayView.swift
+++ Sudoku/How to Play/HowToPlayView.swift
@@ -9,6 +9,13 @@
 import SwiftUI
 
 struct HowToPlayView: View {
+    
+    private var horizontalPadding: CGFloat {
+        return isIpad ? 140 : 4
+    }
+    private var horizontalTextPadding: CGFloat {
+        return isIpad ? 0 : 12
+    }
 
     var body: some View {
         ScrollView {
@@ -17,21 +24,23 @@ struct HowToPlayView: View {
                     Text("Sudoku is a puzzle where you fill all the individual digit squares inside a grid with the right digit. But what determines the right digit? There are a few requirements that must be met:")
                     Text("First, every row in the grid must include the numbers 1 to 9. Each digit can only appear once.")
                 }
-                .padding(.horizontal, 12)
+                .padding(.horizontal, horizontalTextPadding)
                 StaticGridView(highlightSection: .row, grid: GridFactory.easyGrid)
                 Text("Second, every column in the grid must also include the numbers 1 to 9 exactly once.")
-                    .padding(.horizontal, 12)
+                    .padding(.horizontal, horizontalTextPadding)
                 StaticGridView(highlightSection: .column, grid: GridFactory.easyGrid)
                 Text("You'll notice that within the grid there are nine squares shown with bold borders. Below is one square highlighted in yellow. The third requirement is that each of these squares must also only contain the numbers 1 through 9, once per digit.")
-                    .padding(.horizontal, 12)
+                    .padding(.horizontal, horizontalTextPadding)
                 StaticGridView(highlightSection: .square, grid: GridFactory.easyGrid)
                 VStack(alignment: .leading, spacing: 12) {
                     Text("If all three conditions are met, then the Sudoku grid has been solved.")
                     Text("The difficulty of a Sudoku puzzle can range depending on how many digits are already filled in at the beginning. Good luck and have fun solving them!")
                 }
-                .padding(.horizontal, 12)
+                .padding(.horizontal, horizontalTextPadding)
             }
-            .padding(4)
+            .padding(.top, 8)
+            .padding(.bottom, 24)
+            .padding(.horizontal, horizontalPadding)
         }
         .font(Font.system(.headline, design: .rounded))
         .fullBackgroundStyle()
