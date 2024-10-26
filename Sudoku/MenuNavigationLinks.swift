169ebafd873a85711480928e6aa72908f882a123
Ray Kim <rckim77@gmail.com>
2024-10-26 14:41:08 -0400

    refactors menu navigation links

--- Sudoku/MenuNavigationLinks.swift
+++ Sudoku/MenuNavigationLinks.swift
@@ -13,30 +13,17 @@ struct MenuNavigationLinks: View {
     var body: some View {
         #if os(visionOS)
         HStack(spacing: 18) {
-            NavigationLink {
-                StatsView()
-            } label: {
-                Label("Stats", systemImage: "chart.bar")
-                    .font(.system(.headline, design: .rounded))
-                    .tint(Color("dynamicDarkGray"))
-            }
-            NavigationLink {
-                HowToPlayView()
-            } label: {
-                Label("How to play", systemImage: "questionmark.circle")
-                    .font(.system(.headline, design: .rounded))
-                    .tint(Color("dynamicDarkGray"))
-            }
-            NavigationLink {
-                SettingsView()
-            } label: {
-                Label("Settings", systemImage: "gear")
-                    .font(.system(.headline, design: .rounded))
-                    .tint(Color("dynamicDarkGray"))
-            }
+            navigationLinks
         }
         #else
         VStack(spacing: 18) {
+            navigationLinks
+        }
+        #endif
+    }
+    
+    private var navigationLinks: some View {
+        Group {
             NavigationLink {
                 StatsView()
             } label: {
@@ -59,7 +46,6 @@ struct MenuNavigationLinks: View {
                     .tint(Color("dynamicDarkGray"))
             }
         }
-        #endif
     }
 }
 
