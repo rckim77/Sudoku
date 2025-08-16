63c626c6c0346f08314ea05b6d19fde4da5c43be
Ray Kim <rckim77@gmail.com>
2025-08-16 15:20:27 -0400

    adds snapshottesting package, adds basic menu view snapshot tests

---
+++ SudokuTests/SnapshotTests.swift
@@ -0,0 +1,37 @@
+//
+//  SnapshotTests.swift
+//  SudokuTests
+//
+//  Created by Ray Kim on 8/16/25.
+//  Copyright © 2025 Self. All rights reserved.
+//
+
+@testable import Sudoku
+
+import SnapshotTesting
+import Testing
+import SwiftUI
+
+/*
+ 
+Note: The devices will have to be updated over time as we drop iOS support for
+ certain devices and the SnapshotTesting Swift package gets updated with newer
+ screen sizes.
+ 
+.iPhone13 size is equivalent to iPhone 13 Pro, 14, 12 Pro, and 12
+.iPhone13ProMax size is equivalent to iPhone 12 Pro Max and 14 Plus
+.iPhone13Mini size is equivalent to iPhone 12 mini
+
+*/
+@MainActor
+struct SnapshotTests {
+
+    @Test func testMenuViewSnapshots() async throws {
+        let view = MenuView()
+        let vc = UIHostingController(rootView: view)
+
+        assertSnapshot(of: vc, as: .image(on: .iPhone13Mini), named: "iPhone13MiniSize")
+        assertSnapshot(of: vc, as: .image(on: .iPhone13), named: "iPhone13Size")
+        assertSnapshot(of: vc, as: .image(on: .iPhone13ProMax), named: "iPhone13ProMaxSize")
+    }
+}
